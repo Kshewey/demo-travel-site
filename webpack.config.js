57201128edd65c3684c2d8a63fc6c08cddcf89b4
Kevin Shewey <Kevin.Shewey@gmail.com>
2017-02-14 20:57:24 -0800

    Added browser support fallbacks

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-  entry: "./app/assets/scripts/App.js",
+  entry: {
+    App: "./app/assets/scripts/App.js",
+    Vendor: "./app/assets/scripts/Vendor.js"
+  },
   output: {
     path: "./app/temp/scripts",
-    filename: "App.js"
+    filename: "[name].js"
   },
   module: {
     loaders: [
       {
-        loader: 'babel',
+        loader: 'babel-loader',
         query: {
           presets: ['es2015']
         },
